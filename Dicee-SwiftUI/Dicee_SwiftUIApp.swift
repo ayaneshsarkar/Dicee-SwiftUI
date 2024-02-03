a7c6d0835aa6385b46c0d72e9126a1a53ee5d436
Ayanesh Sarkar <ayaneshsarkar101@gmail.com>
2024-02-03 12:24:22 +0530

    Initial Commit

---
+++ Dicee-SwiftUI/Dicee_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dicee_SwiftUIApp.swift
+//  Dicee-SwiftUI
+//
+//  Created by Ayanesh Sarkar on 03/02/24.
+//
+
+import SwiftUI
+
+@main
+struct Dicee_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
